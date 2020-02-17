1173c279674a6493776627687b2660e98ac00b6c
Rory Murray <rory.christian.murray@gmail.com>
2020-02-17 15:00:11 -0500

    Import dependencies

---
+++ test/Inbox.test.js
@@ -0,0 +1,9 @@
+// checks values against each other
+const assert = require('assert');
+
+// local ethereum testnet
+const ganache = require('ganache-cli')
+
+// uppercase as it is a constructor
+// used to create instances of the web3 lib
+const Web3 = require('web3')
