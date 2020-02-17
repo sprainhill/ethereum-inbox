355ade9166d9bc34c5624f9eab518473f91dcdeb
Rory Murray <rory.christian.murray@gmail.com>
2020-02-17 16:17:16 -0500

    Setup initial car test

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -7,3 +7,23 @@ const ganache = require('ganache-cli')
 // uppercase as it is a constructor
 // used to create instances of the web3 lib
 const Web3 = require('web3')
+
+const web3 = new Web3(ganache.provider());
+
+
+class Car {
+    park() {
+        return 'stopped';
+    }
+
+    drive() {
+        return 'vroom';
+    }
+}
+
+describe('Car class', () => {
+    it('car stops', () => {
+        const c1 = new Car()
+        assert.equal(car.park(), 'stopped');
+    })
+})
