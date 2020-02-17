fdc47094d2f4c5fe9f585049adae713a6604c68a
Rory Murray <rory.christian.murray@gmail.com>
2020-02-17 14:20:19 -0500

    Log compiled bytecode for inbox'

---
+++ compile.js
@@ -0,0 +1,12 @@
+// need to read contents of file off hard drive
+const path = require('path') // cross-platform compatibility
+const fs = require('fs')
+const solc = require('solc')
+
+
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol')
+const source = fs.readFileSync(inboxPath, 'utf8');
+
+const compTest = solc.compile(source, 1);
+
+console.log(compTest)
