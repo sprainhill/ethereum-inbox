c0730151f6935f55d6a51f0572e580bc7d682105
Rory Murray <rory.christian.murray@gmail.com>
2020-02-17 20:12:43 -0500

    Default message test

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -8,7 +8,8 @@ const ganache = require('ganache-cli')
 // used to create instances of the web3 lib
 const Web3 = require('web3')
 
-const web3 = new Web3(ganache.provider());
+const provider = ganache.provider();
+const web3 = new Web3(provider);
 
 const { interface, bytecode } = require('../compile');
 
@@ -22,14 +23,24 @@ beforeEach(async () => {
 
     // use one of those accounts
     // to deploy the contract
+    // instance of contract
     inbox = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({ data: bytecode, arguments: ["Hello world."] })
     .send({ from: accounts[0], gas: '1000000' })
+    inbox.setProvider(provider);
 });
 
 describe('Inbox', () => {
     it('deploys a contract', () => {
-        console.log(inbox)
-
+        assert.ok(inbox.options.address);
     });
+
+    it('has a default message', async () => {
+        const message = await inbox.methods.message().call()
+        assert.equal(message, 'Hello world.')
+    });
+
+
+
+
 });
